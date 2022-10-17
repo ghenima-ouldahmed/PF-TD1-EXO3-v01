ed4ddd4bd2bde6bd5bae55dddd6216da332f954d
ghenima-ouldahmed <112975120+ghenima-ouldahmed@users.noreply.github.com>
2022-10-17 16:30:45 +0200

    Create Arbre.java

---
+++ Arbre.java
@@ -0,0 +1,18 @@
+import java.util.Set;
+
+public interface Arbre<T extends Sommable<T>> {
+    int taille();
+
+    boolean contient(final T val);
+
+    Set<T> valeurs();
+
+    T somme();
+
+    T min();
+
+    T max();
+
+    boolean estTrie();
+
+}
